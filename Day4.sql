85b5496dfd04483b42f4ddfee3a19560ba2c29bf
Maheswari <100195819+MAHESWARIYAMANI@users.noreply.github.com>
2025-11-06 20:28:26 +0530

    Add files via upload

---
+++ Day4.sql
@@ -0,0 +1,23 @@
+--1.Display the first 5 patients from the patients table.
+USE HOSPITAL
+SELECT TOP 5 * FROM patients;
+
+--2.Show patients 11-20 using OFFSET.
+SELECT * FROM patients
+ORDER BY patient_id
+OFFSET 10 ROWS
+FETCH NEXT 10 ROWS ONLY;
+
+--3.Get the 10 most recent patient admissions based on arrival_date.
+SELECT TOP 10 * FROM patients
+ORDER BY arrival_date DESC;
+
+--#DAILY CHALLENGE 
+/*Find the 3rd to 7th highest patient satisfaction scores from the
+patients table, showing patient_id, name, service, and satisfaction.
+Display only these 5 records.*/
+SELECT patient_id, name, service, satisfaction 
+FROM patients
+ORDER BY satisfaction DESC
+OFFSET 2 ROWS
+FETCH NEXT 5 ROWS ONLY;
