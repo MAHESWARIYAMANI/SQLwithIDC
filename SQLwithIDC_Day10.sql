6b21b29a42f68adb34a0453a59284f1c0184f753
Maheswari <100195819+MAHESWARIYAMANI@users.noreply.github.com>
2025-11-14 16:56:40 +0530

    Day 10

--- SQLwithIDC_Day10.sql
+++ SQLwithIDC_Day10.sql
@@ -16,3 +16,28 @@ SELECT
 FROM services_weekly
 GROUP BY service
 ORDER BY average_satisfaction DESC;
+
+--1.Categorise patients as 'High', 'Medium', or 'Low' satisfaction based on their scores.
+USE HOSPITAL;
+SELECT * ,
+CASE WHEN satisfaction >=85 THEN 'Excellent'
+     WHEN satisfaction >=75 THEN 'Good'
+     WHEN satisfaction >=65 THEN 'Fair'
+     ELSE 'Needs Improvement'
+     END AS satisfaction_category
+FROM patients;
+
+--2.Label staff roles as 'Medical' or 'Support' based on role type.
+SELECT staff_id,staff_name, 
+CASE WHEN role = 'nurse' THEN 'Support'
+     WHEN role = 'nursing_assistant' THEN 'Support'
+     ELSE 'Medical' END AS role_type
+FROM staff;
+
+--3.Create age groups for patients (0-18, 19-40, 41-65, 65+).
+SELECT name, age,
+CASE WHEN age BETWEEN 0 AND 18 THEN 'Children'
+     WHEN age BETWEEN 19 AND 40 THEN 'Young Adults'
+     WHEN age BETWEEN 41 AND 65 THEN 'MIddle Aged Adults'
+     ELSE 'Senior' END AS age_group
+FROM patients;
